470830a2ff22ba82033273f54e6b211c62d2bd0e
iamgauraav <iamgauraav@gmail.com>
2022-04-18 21:43:14 +0530

    Added UC2 : Added contacts in address book

---
+++ AddressBook_CS/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddressBook_CS
+{
+    class Contact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string email { get; set; }
+        public long phoneNumber { get; set; }
+    }
+}
