42980c03674c1da04ff9c1f7cfdda36ab11e2966
iamgauraav <iamgauraav@gmail.com>
2022-04-18 19:34:36 +0530

    Added UC1 - To create contacts in addressbook

--- AddressBook_CS/Program.cs
+++ AddressBook_CS/Program.cs
@@ -2,11 +2,33 @@
 
 namespace AddressBook_CS
 {
-    class Program
+    class Contacts
     {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string zip;
+        public string email;
+        public long phoneNumber;
+
+        public Contacts(string firstName, string lastName, string address, string city, string state, string zip, string email, long phoneNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+            
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book System program master branch");
-        }
+            Contacts contacts = new Contacts("Gaurav", "Rawat", "Indore", "Indore", "M.P", "452001", "hhhghggg@gmail.com", 9434349222 );
+        }     
     }
 }
